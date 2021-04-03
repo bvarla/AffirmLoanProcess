eebcb46cb0dae7871570130f5954eb6cd484cdaa
bvarla <rezz4tech@gmail.com>
2021-04-03 00:27:46 -0400

    Initial Commit

---
+++ src/main/java/com/example/AffirmLoanProcess/AffirmLoanProcessApplication.java
@@ -0,0 +1,13 @@
+package com.example.AffirmLoanProcess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffirmLoanProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffirmLoanProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AffirmLoanProcess/AffirmLoanProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AffirmLoanProcess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffirmLoanProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
